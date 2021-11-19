9af42adfb8cf1d84d4a8fa0c7323dafd2f7c7464
Angélica Sofía Pagés Burgos <68571768+angelicapages@users.noreply.github.com>
2021-11-19 10:42:14 -0300

    switch

--- BICI/app.js
+++ BICI/app.js
@@ -1,26 +1,56 @@
-const imagenMostrada = document.querySelector(".imagenMostradaJs");
+const imagenesMostrada = document.querySelectorAll(".imagenMostradaJs");
 const colores = document.querySelectorAll(".coloresJs");
 const rodados = document.querySelectorAll(".rodado")
+const cards = document.querySelectorAll(".card")
+
+
+const cambioDeImagen = (id) => {
+    imagenesMostrada.forEach(imagenMostrada => {
+        switch (id) {
+            case :
+
+                break;
+            case :
+
+                break;
+            case :
+
+                break;
+            case :
+
+                break;
+            case :
+
+                break;
+
+            default:
+                break;
+        }
+        imagenMostrada.setAttribute("src", `./img/${id}.png`)
+
+    })
+}
+
+
 colores.forEach(color => {
     color.addEventListener("click", () => {
-        const id = color.getAttribute("id")
-        imagenMostrada.setAttribute("src",`./img/${id}.png`)
-        console.log(imagenMostrada)
+        let id = color.getAttribute("id")
+        cambioDeImagen(id)
     })
 })
-console.log(rodados)
+
+
 const eliminarActive = () => {
     rodados.forEach(rodado => {
         rodado.classList.remove("active")
     })
-    
+
 }
 rodados.forEach(rodado => {
     rodado.addEventListener("click", () => {
-    eliminarActive()   
-    rodado.classList.add("active")
+        eliminarActive()
+        rodado.classList.add("active")
 
     })
 })
-
 
