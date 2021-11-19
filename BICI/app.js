fd333f8be7dda8be9556b360eb818d04731d1db2
Christiang98 <chrisu9855@gmail.com>
2021-11-19 20:52:35 -0300

    Targetas bicis terminadas falta responsividad

--- BICI/app.js
+++ BICI/app.js
@@ -1,35 +1,87 @@
-const imagenesMostrada = document.querySelectorAll(".imagenMostradaJs");
+const imagenMostrada = document.querySelector(".imagenMostradaBici")
+const imagenMostrada2 = document.querySelector(".imagenMostradaBici2")
+const imagenMostrada3 = document.querySelector(".imagenMostradaBici3")
+const imagenMostrada4 = document.querySelector(".imagenMostradaBici4")
+const imagenMostrada5 = document.querySelector(".imagenMostradaBici5")
+const imagenMostrada6 = document.querySelector(".imagenMostradaBici6")
+const imagenMostrada7 = document.querySelector(".imagenMostradaCasco")
 const colores = document.querySelectorAll(".coloresJs");
 const rodados = document.querySelectorAll(".rodado")
 const cards = document.querySelectorAll(".card")
 
 
 const cambioDeImagen = (id) => {
-    imagenesMostrada.forEach(imagenMostrada => {
-        switch (id) {
-            case :
+    switch (id) {
+        case "roja":
+            imagenMostrada.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "amarilla":
+            imagenMostrada.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "verde":
+            imagenMostrada.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "naranja":
+            imagenMostrada2.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "blanca":
+            imagenMostrada2.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "celeste":
+            imagenMostrada2.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "azul":
+            imagenMostrada3.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "fluor":
+            imagenMostrada3.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "gris":
+            imagenMostrada3.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "marron":
+            imagenMostrada4.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "cyan":
+            imagenMostrada4.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "bordo":
+            imagenMostrada4.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "amarillita":
+            imagenMostrada5.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "grisesita":
+            imagenMostrada5.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "blanquita":
+            imagenMostrada5.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "negra":
+            imagenMostrada6.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "rosa":
+            imagenMostrada6.setAttribute("src", `./img/${id}.png`)
+            break;
+        case "manzana":
+            imagenMostrada6.setAttribute("src", `./img/${id}.png`)
+            break;
+        // case "rojo":
+        //     imagenMostrada7.setAttribute("src", `./img/${id}.png`)
+        //     break;
+        // case "rosado":
+        //     imagenMostrada7.setAttribute("src", `./img/${id}.png`)
+        //     break;
+        // case "naranjita":
+        //     imagenMostrada7.setAttribute("src", `./img/${id}.png`)
+        //     break;
+        default:
+            break;
+    }
 
-                break;
-            case :
 
-                break;
-            case :
-
-                break;
-            case :
-
-                break;
-            case :
-
-                break;
-
-            default:
-                break;
-        }
-        imagenMostrada.setAttribute("src", `./img/${id}.png`)
-
-    })
 }
+
 
 
 colores.forEach(color => {
